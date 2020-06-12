c4b7d7d63ca55070ee39c2053ed44ac15097143c
MakersAll8 <lixiao.shanghai@gmail.com>
2020-06-12 11:57:56 +1000

    added comments

--- src/engineer/xiao/thermal/Main.java
+++ src/engineer/xiao/thermal/Main.java
@@ -10,7 +10,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
+        // create an array list of product lines
+        // 创建代表产品列表每一行的一个数组
         ArrayList<ProductLine> productLines = new ArrayList<>();
         productLines.add(
                 new ProductLine("Nike Flight 89", "耐克男子篮球鞋", 170.0, 2.00)
@@ -22,7 +23,8 @@ public class Main {
                 new ProductLine("Nike Air Force 1 High", "耐克空军一号高帮", 190.0, 3.00)
         );
 
-        // load image
+        // load logo and qr code
+        // 加载公司图标和二维码到内存
         BufferedImage bufferedImage = ImageIO.read(new File("./taolu.png"));
         byte[] blackWhitePix = ZJ58.processImage(bufferedImage);
         byte[] logo = ZJ58.eachLinePixToCmd(blackWhitePix, ZJ58.printWidth, ZJ58.nMode);
@@ -31,14 +33,25 @@ public class Main {
         byte[] qrPix = ZJ58.processImage(qrImage);
         byte[] qrCode = ZJ58.eachLinePixToCmd(qrPix, ZJ58.printWidth, ZJ58.nMode);
 
+        // open stream connection to line printer
+        // 打开连接打印机的数据流
         FileOutputStream fs = new FileOutputStream("/dev/usb/lp2");
         PrintStream ps = new PrintStream(fs);
+        // sending hex code to initialize the thermal printer. Note, this clears
+        // encoding settings as well. I believe default is ASCII.
+        // 发送初始化的16进制代码到热敏打印机。注意，这会把字符编码也重置。默认编码应该是ASCII
         ps.write(ZJ58.init);
+        // set encoding to GBK to print Chinese
+        // 设置编码为GBK打印中文
         ps.write(ZJ58.zh_mode);
 
+        // print logo
+        // 打印公司图标
         ps.write(logo);
         ps.write(ZJ58.print_and_roll);
 
+        // print product lines
+        // 打印每一行产品列表
         double total = 0;
         for (ProductLine productLine : productLines) {
             byte[] byteArray = productLine.toString().getBytes("GBK");
@@ -47,17 +60,23 @@ public class Main {
             total += productLine.getTotal();
         }
 
+        // print total
+        // 打印总计
         String totalString = "Total 总计： " + total;
         ps.write(totalString.getBytes("GBK"));
         ps.write(ZJ58.print_and_roll);
 
+        // print qr code
+        // 打印二维码
         ps.write(qrCode);
         ps.write(ZJ58.clean_end);
 
-//        ps.write(reset);
-//        ps.write(reset);
-
+        // send all hex data stream
+        // 发送所有16进制数据流
         ps.flush();
+        // close the stream
+        // 关闭数据流
+        ps.close();
     }
 
 
--- src/engineer/xiao/thermal/README.txt
+++ src/engineer/xiao/thermal/README.txt
@@ -1,10 +1,12 @@
 This is a demo to print receipts on USB zj58 thermal printers from Ubuntu
+
 This demo is not an official demo, it is a simple hack based on my limited
 understanding of the official android demo.
-It is expected that you run into Permission Denied exception if your user does
+
+It is expected that you run into Permission Denied Exception if your user does
 not have permission to access the printer resource on your OS. Open up your
 command line and run
-  - sudo usermod -a -G lp <ubuntu_user>
+  - sudo usermod -a -G lp <your_ubuntu_username>
 Log out and log in, or reboot.
 
 Also, your Thermal printer might not be mounted at /dev/usb/lp2 like mine. Run
@@ -17,3 +19,20 @@ adapt code you need to achieve alignment.
 
 Feel free to use, or modify this repo in any way you want. I am not liable to
 any harm or damages caused in anyway.
+
+这个Demo展示了如何从Ubuntu系统通过资江58热敏打印机打印小票。
+
+这个Demo不是官方的Demo，而是基于我对官方安卓Demo有限的理解做的一个简单粗暴版本。
+
+你直接跑这个程序的话会遇到Permission Denied Exception权限不够。打开命令行跑
+  - sudo usermod -a -G lp <你的用户名>
+退出、登录、或者重启。
+
+此外，你的热敏打印机不一定挂载在 /dev/usb/lp2 这个路径。跑
+  - ll /dev/usb/ 来查看你有几台打印机
+  - lpstat -s 也可能有帮助
+
+注意：（NB不是牛X）官方提供了16进制代码来实现居中、靠右等等这些操作。大家可以参考官方安卓的Demo
+来实现。实现起来很容易。
+
+这些代码你可以随意使用、修改。我对任何因此带来的任何伤害或者损坏不承担责任。
